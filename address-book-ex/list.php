3e212370cf388565877ed32a23a97ab63149ba5f
amy520520@example.com <amy520520>
2023-05-02 17:14:41 +0800

    20230502 17:14

--- address-book-ex/list.php
+++ address-book-ex/list.php
@@ -20,7 +20,7 @@ $row = []; # 分頁資料
 $t_sql = "SELECT COUNT(1) FROM address_book" ;
 
 //計算總筆數
-$tota1Rows =$pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0]; 
+$totalRows =$pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0]; 
 //計算總頁數
 if($totalRows > 0){
     $totalPages = ceil($totalRows / $perPage);
@@ -28,13 +28,13 @@ if($totalRows > 0){
     header("Location: list.php?page={$totalPages}");
     exit;
     }
-    $sql = sprintf("SELECT * FROM `address_book` LIMIT %s,%s",($page-1)*$perPage, $perPage);
+    $sql = sprintf("SELECT * FROM `address_book`ORDER BY sid DESC LIMIT %s,%s",($page-1)*$perPage, $perPage);
     $rows = $pdo->query($sql)->fetchAll();
 }
 
 
 echo json_encode([
-    'totalRows' => $tota1Rows,
+    'totalRows' => $totalRows,
     'totalPages' => $totalPages,
     'page' => $page,
     'rows'=> $rows,
@@ -49,7 +49,36 @@ exit; //結束 php 程式執行
 
 <?php include './parts/navbar.php' ?>
 
-  <div class="container"></div>
+  <div class="container">
+    <div class="row">
+        <div class="col">
+            <table class="table">
+                <thead>
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">姓名</th>
+                    <th scope="col">手機</th>
+                    <th scope="col">電郵</th>
+                    <th scope="col">生日</th>
+                    <th scope="col">地址</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($rows as $r): ?>
+                    <tr>
+                        <td><?= $r['sid'] ?></td>
+                        <td><?= $r['name'] ?></td>
+                        <td><?= $r['mobile'] ?></td>
+                        <td><?= $r['email'] ?></td>
+                        <td><?= $r['birthday'] ?></td>
+                        <td><?= $r['address'] ?></td>
+                    </tr>
+                    <?php endforeach ?>
+                </tbody>
+            </table>            
+        </div>
+    </div>
+  </div>
 
 
     
